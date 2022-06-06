aaedcef33eb4fa5479b1fbe55ab4dfd65689f1fa
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-06-06 14:23:41 +0700

    Add minor

--- app/src/main/java/com/example/recycraft/data/local/DataPreferences.kt
+++ app/src/main/java/com/example/recycraft/data/local/DataPreferences.kt
@@ -4,7 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import com.example.recycraft.data.remote.Session
+//import com.example.recycraft.data.remote.Session
 import kotlinx.coroutines.flow.map
 import java.util.concurrent.Flow
 import java.util.prefs.Preferences
@@ -14,21 +14,21 @@ class DataPreferences private constructor(private val dataStore: DataStore<Prefe
     private val loginKey = booleanPreferencesKey("login_setting")
     private val tokenKey = stringPreferencesKey("token_key")
 
-    fun getLoginSetting() : Flow<Session>{
-        return dataStore.data.map { preferences ->
-            Session(
-                preferences[tokenKey] ?: "",
-                preferences[loginKey] ?:false,
-            )
-        }
-    }
-
-    suspend fun saveLoginSetting(isLogin: Boolean, token: String){
-        dataStore.edit { preferences ->
-            preferences[loginKey] = isLogin
-            preferences[tokenKey] = token
-        }
-    }
+//    fun getLoginSetting() : Flow<Session>{
+//        return dataStore.data.map { preferences ->
+//            Session(
+//                preferences[tokenKey] ?: "",
+//                preferences[loginKey] ?:false,
+//            )
+//        }
+//    }
+//
+////    suspend fun saveLoginSetting(isLogin: Boolean, token: String){
+//        dataStore.edit { preferences ->
+//            preferences[loginKey] = isLogin
+//            preferences[tokenKey] = token
+//        }
+//    }
 
     companion object {
         @Volatile
